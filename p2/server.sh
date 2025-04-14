e01ce685def83148b8504b8836929a03a3dac480
gfranque42 <gfranque@student.42.fr>
2025-04-14 15:41:51 +0200

    🚚 build: add the webapps command in server.sh and move the webapps.yaml in src

--- p2/server.sh
+++ p2/server.sh
@@ -4,3 +4,4 @@ apt install -y curl
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--token 12354 --write-kubeconfig-mode 0644 --node-ip 192.168.56.110" sh -s -
 
+kubectl apply -f /home/webapps.yaml
