2bc88441de40e4ab36852ebb2c14eae67e422ce3
Stanouuu <stanislas.barrage@gmail.com>
2025-05-05 10:36:21 +0200

    wip: server.sh wip

--- p3/server.sh
+++ p3/server.sh
@@ -1,6 +1,6 @@
 apt update
-# apt upgrade -y
-apt install -y curl ca-certificates curl
+apt install -y curl ca-certificates curl apt-transport-https gnupg
+
 
 
 
@@ -23,18 +23,25 @@ VERSION_STRING=5:28.1.1-1~debian.11~bullseye
 sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
+
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version --client
-
 k3d cluster create my-cluster --api-port 6443 -p 8080:80@loadbalancer --agents 2
 
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+# kubectl config current-context
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
 
