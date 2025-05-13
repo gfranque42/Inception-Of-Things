52124137a1a9995cffd9d6c16b1424319270c06c
Ariti-87 <ariti87@hotmail.com>
2025-05-13 15:17:19 +0200

    starting p3

--- p1/agent.sh
+++
@@ -1,4 +0,0 @@
-apt update
-# apt upgrade -y
-apt install -y curl
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 12354 --node-ip 192.168.56.111" sh -s -
