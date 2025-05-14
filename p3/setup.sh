ce600339ca5d001d51411f811223a6f79652b311
stanouuu <stanislas.barrage@gmail.com>
2025-05-14 16:16:57 +0200

    i need help

--- p3/setup.sh
+++ p3/setup.sh
@@ -3,7 +3,7 @@
 # newgrp docker
 
 # k3d setup
-k3d cluster create my-cluster --api-port 6443 --port 8080:80@loadbalancer --port 8443:443@loadbalancer --agents 2
+k3d cluster create my-cluster --api-port 6443 --agents 2
 kubectl config use-context k3d-my-cluster
 
 # k3d argocd setup
@@ -13,11 +13,13 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl config set-context --current --namespace=argocd
 kubectl wait --for=condition=Ready pods --all -n argocd --timeout=600s
 kubectl get all 
-kubectl port-forward svc/argocd-server -n argocd 8083:443 &
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # argocd CLI setup
-# argocd cluster add -y k3d-my-cluster
+ARGOCD_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode)
+argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+# argocd cluster add -y k3d-my-cluster --server localhost:8080 --insecure
 # kubectl config set-context --current --namespace=argocd
 # argocd app create playground --repo https://github.com/gfranque42/Inception-Of-Things.git --path p3/app --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8083 --insecure
 
