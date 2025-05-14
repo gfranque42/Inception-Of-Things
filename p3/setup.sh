4c81c0b7fc15358e1a99167ebe89f0f0d85d2b21
stanouuu <stanislas.barrage@gmail.com>
2025-05-14 18:39:21 +0200

    fix: add .yaml updated for new tests

--- p3/setup.sh
+++ p3/setup.sh
@@ -18,11 +18,12 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 
 # argocd CLI setup
 ARGOCD_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode)
+echo $ARGOCD_PASSWORD
 argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
-# argocd cluster add -y k3d-my-cluster --server localhost:8080 --insecure
-# kubectl config set-context --current --namespace=argocd
-# argocd app create playground --repo https://github.com/gfranque42/Inception-Of-Things.git --path p3/app --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8083 --insecure
+argocd cluster add -y k3d-my-cluster --server localhost:8080 --insecure
+kubectl config set-context --current --namespace=argocd
+argocd app create playground --repo https://github.com/gfranque42/Inception-Of-Things.git --path p3/app --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8080 --insecure
 
 # # argocd account update-password
-# argocd app sync playground
-# argocd app get playground
+argocd app sync playground
+argocd app get playground
