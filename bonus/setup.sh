3e5cf512f8e58d3b5e303b752a153b162357fbae
arincon <arincon@IOT.myguest.virtualbox.org>
2025-05-20 17:50:57 +0200

    values.yaml reworked

--- bonus/setup.sh
+++ bonus/setup.sh
@@ -1,6 +1,3 @@
-# sudo usermod -aG docker $USER
-# newgrp docker
-
 # k3d setup
 k3d cluster create my-cluster --api-port 6443 --agents 2
 kubectl config use-context k3d-my-cluster
@@ -9,7 +6,6 @@ kubectl config use-context k3d-my-cluster
 kubectl create namespace argocd
 kubectl create namespace dev
 kubectl apply -n argocd -f ./install.yaml
-# kubectl config set-context --current --namespace=argocd
 kubectl wait --for=condition=Ready pods --all -n argocd --timeout=600s
 kubectl get all
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
@@ -17,10 +13,9 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}
 
 # argocd CLI setup
 ARGOCD_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode)
-echo $ARGOCD_PASSWORD
+echo "ARGO PASSWORD: $ARGOCD_PASSWORD"
 argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
 argocd cluster add -y k3d-my-cluster --server localhost:8080 --insecure
-# kubectl config set-context --current --namespace=argocd
 # argocd app create gfranque-playground --repo https://github.com/gfranque42/gfranque.git --path playground/ --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8080 --insecure --upsert
 
 
@@ -34,8 +29,14 @@ argocd cluster add -y k3d-my-cluster --server localhost:8080 --insecure
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 kubectl create namespace gitlab
-helm install gitlab gitlab/gitlab --namespace gitlab --values values.yaml
+# helm install gitlab gitlab/gitlab --namespace gitlab -f values.yaml
+helm upgrade --install gitlab gitlab/gitlab \
+  --namespace gitlab \
+  -f values.yaml \
+  --set global.hosts.domain=local.gitlab.com \
+  --set global.hosts.externalIP=0.0.0.0 \
+  --set global.hosts.https=false
 kubectl wait --for=condition=ready pod -l app=webservice -n gitlab --timeout=600s
 kubectl port-forward svc/gitlab-webservice-default 8081:8080 -n gitlab &
-echo "GitLab password:"
+echo "GITLAB PASSWORD:"
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d && echo
