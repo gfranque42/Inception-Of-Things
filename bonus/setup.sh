dca948cc3886feef33babdf883a2407e52a032be
arincon <arincon@IOT.myguest.virtualbox.org>
2025-05-28 17:15:28 +0200

    add sudo to docker cmd and change repo git

--- bonus/setup.sh
+++ bonus/setup.sh
@@ -16,7 +16,7 @@ ARGOCD_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o js
 echo "Argocd Password: $ARGOCD_PASSWORD"
 argocd login localhost:8082 --username admin --password "$ARGOCD_PASSWORD" --insecure
 argocd cluster add -y k3d-my-cluster --server localhost:8082 --insecure
-argocd app create gfranque-playground --repo http://host.k3d.internal:8080/root/test2.git --path playground/ --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8082 --insecure --upsert
+argocd app create gfranque-playground --repo http://host.k3d.internal:8080/root/gfranque.git --path playground/ --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8082 --insecure --upsert
 
 # argocd account update-password
 argocd app sync gfranque-playground
