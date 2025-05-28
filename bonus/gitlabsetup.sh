ef31a813f0266e8f0804c1e1c73490445b6d0bff
gfranque42 <gfranque@student.42.fr>
2025-05-28 16:03:46 +0200

    🐛 fix: fix the get token to allow a push in the script

--- bonus/gitlabsetup.sh
+++ bonus/gitlabsetup.sh
@@ -3,18 +3,23 @@
 PASSWORD=`docker exec -it gitlab cat /etc/gitlab/initial_root_password | grep "Password:" | awk '{ print $2 }'`
 echo "ADMIN PASSWORD = $PASSWORD"
 docker cp ./setup.rb gitlab:/tmp/setup.rb
-TOKEN=`docker exec -it gitlab gitlab-rails runner /tmp/setup.rb | awk '{ print $3 }'` ; echo $TOKEN
+echo -n "http://root:" > bob
+TOKEN=`docker exec gitlab gitlab-rails runner /tmp/setup.rb | awk '{ print $3 }'`
+echo -n $TOKEN >> bob
+echo "@localhost:8080/root/gfranque.git" >> bob
+REPO_URL=`tr -d '\n' < bob`
+echo "Token: $TOKEN"
+echo "Repo url: $REPO_URL"
 curl --request POST \
   --header "PRIVATE-TOKEN: $TOKEN" \
   "http://localhost:8080/api/v4/projects?name=gfranque&description=blablablaiotblablabla&visibility=public"
-REPO_URL="http://root:$TOKEN@localhost:8080/root/gfranque.git"
 mkdir -p gfranque
-cp -r app ./gfranque
+cp -r playground ./gfranque
 cd gfranque
 git init
 git remote add origin "$REPO_URL"
-git add app
-git commit -m "feat: add app"
+git add playground
+git commit -m "feat: add playground"
 git push -u origin master
 cd ..
 rm -rf gfranque
