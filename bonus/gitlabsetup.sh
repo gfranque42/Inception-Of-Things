8cceae9ce01f4c7d5a3f9cb34058282ec5ef122f
arincon <arincon@IOT.myguest.virtualbox.org>
2025-06-04 16:40:39 +0200

    add password.txt and wait for deployement/playground

--- bonus/gitlabsetup.sh
+++ bonus/gitlabsetup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-PASSWORD=`sudo docker exec -it gitlab cat /etc/gitlab/initial_root_password | grep "Password:" | awk '{ print $2 }'`
-echo "ADMIN PASSWORD = $PASSWORD"
+echo -n "gitlab: " > password.txt
+sudo docker exec -it gitlab cat /etc/gitlab/initial_root_password | grep "Password:" | awk '{ print $2 }' >> password.txt
+# PASSWORD=`sudo docker exec -it gitlab cat /etc/gitlab/initial_root_password | grep "Password:" | awk '{ print $2 }'`
+# echo "ADMIN PASSWORD = $PASSWORD"
 sudo docker cp ./setup.rb gitlab:/tmp/setup.rb
 echo -n "http://root:" > bob
 TOKEN=`sudo docker exec gitlab gitlab-rails runner /tmp/setup.rb | awk '{ print $3 }'`
--- bonus/setup.sh
+++ bonus/setup.sh
@@ -12,8 +12,9 @@ kubectl port-forward svc/argocd-server -n argocd 8082:443 &
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # argocd CLI setup
+echo -n "argocd: " >> password.txt
+kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode >> password.txt
 ARGOCD_PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode)
-echo "Argocd Password: $ARGOCD_PASSWORD"
 argocd login localhost:8082 --username admin --password "$ARGOCD_PASSWORD" --insecure
 argocd cluster add -y k3d-my-cluster --server localhost:8082 --insecure
 argocd app create gfranque-playground --repo http://host.k3d.internal:8080/root/gfranque.git --path playground/ --dest-server https://kubernetes.default.svc --dest-namespace dev --server localhost:8082 --insecure --upsert
@@ -22,4 +23,5 @@ argocd app create gfranque-playground --repo http://host.k3d.internal:8080/root/
 argocd app sync gfranque-playground
 argocd app get gfranque-playground
 argocd app set gfranque-playground --sync-policy automated
+kubectl wait --for=condition=available deployment/playground -n dev --timeout=600s
 kubectl port-forward svc/playground-service 8888:80 -n dev &
